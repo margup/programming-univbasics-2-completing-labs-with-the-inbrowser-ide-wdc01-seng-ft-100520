208308090195716b67c059568db3368f3c4110b5
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-08 15:10:55 +0000

    Automatically backed up by Learn

--- lib/your_solution.rb
+++ lib/your_solution.rb
@@ -1,6 +1,10 @@
 # Write your solution here
+<<<<<<< HEAD
 def greeting(hi= "Hello World!")
  puts "#{hi}"
 end
 greeting
+=======
+puts " "
+>>>>>>> 155b85c6ff82dd1eb0bc0fc8b19d455cf8333583
 # This file should output "Hello World!" using a puts statement
